040139cfd7dcfd5181df83ffbfc1aaac61c4cbb3
ryuzaki01 <shinji666mmc@gmail.com>
2016-05-24 13:17:05 +0700

    + Revert

--- IntrusiveAdsCleaner.js
+++ IntrusiveAdsCleaner.js
@@ -5,46 +5,39 @@
  *  @author Ihsan Fauzi Rahman <shinji666mmc@gmail.com>
  */
 
-(function (__global) {
-  var doc = (__global.top || __global.parent) ? (__global.top.document || __global.parent.document) : document;
-  var checkCountDown = 5;
+var doc = (window.top || window.parent) ? (window.top.document || window.parent.document) : document;
+var checkCountDown = 5;
 
-  // Die you telkom
-  (function (w, d, s) {
-    d.write('<!--');
+// Die you telkom
+(function (w, d, s) {
+  d.write('<!--');
 
-    // Just in case above function didn't work
-    var checkIntrusiveAdsInterval = w.setInterval(function () {
-      var telkomTopAds = d.getElementById('cfs_top_div');
-      var telkomBottomAds = d.getElementById('cfs_div_2');
-      var head = d.getElementsByTagName('head')[0];
-      if (telkomBottomAds) {
-        var parent = telkomBottomAds.parentNode;
+  // Just in case above function didn't work
+  var checkIntrusiveAdsInterval = w.setInterval(function () {
+    var telkomTopAds = d.getElementById('cfs_top_div');
+    var telkomBottomAds = d.getElementById('cfs_div_2');
+    var head = d.getElementsByTagName('head')[0];
+    if (telkomBottomAds) {
+      var parent = telkomBottomAds.parentNode;
 
-        while (telkomBottomAds.firstChild) {
-          parent.insertBefore(telkomBottomAds.firstChild, telkomBottomAds);
-        }
-
-        // And then we can remove all the telkom things
-        parent.removeChild(telkomTopAds);
-        parent.removeChild(telkomBottomAds);
-        head.removeChild(d.querySelector(s + '[src^="http://cfs.uzone.id"]'));
-
-        // We than stop our interval
-        w.clearInterval(checkIntrusiveAdsInterval);
+      while (telkomBottomAds.firstChild) {
+        parent.insertBefore(telkomBottomAds.firstChild, telkomBottomAds);
       }
 
-      if (checkCountDown <= 0) {
-        // We than stop our interval
-        w.clearInterval(checkIntrusiveAdsInterval);
-      } else {
-        checkCountDown--;
-      }
-    }, 3000);
-  })(__global, doc, 'script');
-})(
-  typeof window !== 'undefined' ? window :
-  typeof WorkerGlobalScope !== 'undefined' ? self :
-  typeof global !== 'undefined' ? global :
-  Function('return this;')()
-);
+      // And then we can remove all the telkom things
+      parent.removeChild(telkomTopAds);
+      parent.removeChild(telkomBottomAds);
+      head.removeChild(d.querySelector(s + '[src^="http://cfs.uzone.id"]'));
+
+      // We than stop our interval
+      w.clearInterval(checkIntrusiveAdsInterval);
+    }
+
+    if (checkCountDown <= 0) {
+      // We than stop our interval
+      w.clearInterval(checkIntrusiveAdsInterval);
+    } else {
+      checkCountDown--;
+    }
+  }, 3000);
+})(window, doc, 'script');
